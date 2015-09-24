1a0fd58f2b04953552331f506138a7484ad853e7
Valeriya <lerxen13@yandex.ru>
2015-09-24 21:19:48 +0300

    Update program.py

--- lab3/Grushitcyna/program.py
+++ lab3/Grushitcyna/program.py
@@ -4,11 +4,11 @@ first_line = stdin.readline()
 n = int(first_line)
 second_line = stdin.readline()
 m = [int(s) for s in second_line.split(' ')]
-kindsOfCoins = m.__len__()
+kindsOfCoins = len(m)
 
 
 def numberOfVariants(n):
-    return countFunction(n, kindsOfCoins - 1)
+    return countFunction(n, kindsOfCoins)
 
 
 def countFunction(n, kindsOfCoins):
@@ -17,10 +17,8 @@ def countFunction(n, kindsOfCoins):
     elif n < 0 or kindsOfCoins == 0:
         return 0
     else:
-        return countFunction(n, kindsOfCoins - 2) + countFunction(n - m[kindsOfCoins - 1], kindsOfCoins)
+        return countFunction(n, kindsOfCoins - 1) + countFunction(n - m[kindsOfCoins - 1], kindsOfCoins)
 
 
-print(numberOfVariants(n))
-print(kindsOfCoins)
-print(m)
-print(m[1])
+
+print numberOfVariants(n)
