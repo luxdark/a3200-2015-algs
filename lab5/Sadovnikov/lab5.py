7d2f0f378aee1377be7adb2aa79549442d3db059
Alexander <Dongodfather@mail.ru>
2015-09-26 12:42:30 +0300

    lab5 finally working

--- lab5/Sadovnikov/lab5.py
+++ lab5/Sadovnikov/lab5.py
@@ -1,35 +1,32 @@
 from sys import stdin
 from random import randint
 
-line = stdin.readline()
-input = [int(s) for s in line.split(' ')]
+
+input = [randint(0, 10000) for i in range (100000)]
 
 
 def quick_sort(array, beginning_index, last_index):
     if beginning_index < last_index:
-        new_ending_index, new_beginning_index = partition(array, beginning_index, last_index)
+        new_ending_index, new_beginning_index = randomized_partition(array, beginning_index, last_index)
         quick_sort(array, beginning_index, new_ending_index)
         quick_sort(array, new_beginning_index + 1, last_index)
 
 
 def partition(array, beginning_index, last_index):
-    list_of_indexes = []
     default_variable = array[last_index]
     i = beginning_index - 1
-    k = 0
+    k = beginning_index
     for j in range(beginning_index, last_index):
         if array[j] <= default_variable:
             i += 1
-            if array[j] == default_variable:
-                list_of_indexes.append(i)
             array[i], array[j] = array[j], array[i]
+            if array[i] == default_variable:
+                array[i], array[k] = array[k], array[i]
+                k += 1
     array[i + 1], array[last_index] = array[last_index], array[i + 1]
     j = i
-    for x in list_of_indexes:
-        if array[j] != default_variable:
-            array[x], array[j] = array[j], array[x]
-        else:
-            list_of_indexes.remove(j)
+    for x in range(beginning_index, k):
+        array[j], array[x] = array[x], array[j]
         j -= 1
     return j, i + 1
 
